dce4fc57b58553c0f4435f1c8d828e8f23fbca8d
kbegot <begot.kevin@gmail.com>
2023-07-12 14:49:06 +0200

    [ADD] info data

--- src/views/Tchek.vue
+++ src/views/Tchek.vue
@@ -1,39 +1,48 @@
 <template>
     <div>
         <h1 class="text-center bold">IFrame Tchek Testing</h1>
-        <iframe class="w-full h-[850px]" :class="[!isValide ? 'block' : 'hidden']" id="iframe" :src="iframeSrc"
-        allow="camera *;microphone *"></iframe>
+        <ul> {{ info.data.tchek.report.clientLastName }}
+            <li v-for="(value, key) in info" :key="key">
+                {{ key }}: {{ value }}
+            </li>
+        </ul>
+            <iframe class="w-full h-[850px]" :class="[!isValide ? 'block' : 'hidden']" id="iframe" :src="iframeSrc"
+                allow="camera *;microphone *"></iframe>
     </div>
     <div class="flex justify-center pt-2 bg-white">
-        <button class="p-3 text-white bg-green-600 rounded-md" :class="[!isValide ? 'block' : 'hidden']" @click="valider">Valider</button>
-        <a class="p-3 text-white bg-green-600 rounded-md" :class="[isValide ? 'block' : 'hidden']" :href="rapportSrc">Cliquez pour voir le rapport</a>
+        <button class="p-3 text-white bg-green-600 rounded-md" :class="[!isValide ? 'block' : 'hidden']"
+            @click="valider">Valider</button>
+        <a class="p-3 text-white bg-green-600 rounded-md" :class="[isValide ? 'block' : 'hidden']"
+            :href="rapportSrc">Cliquez pour voir le rapport</a>
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
-
 const isValide = ref(false);
 const iframeSrc = ref('');
 const rapportSrc = ref('');
+const info = ref('');
 let token = '';
+
 
 const valider = async () => {
     isValide.value = true;
     try {
         const response = await axios.get('https://alto.tchek.fr/apiV1/report/loadtchek', {
             params: {
-            token: token
-        },
+                token: token
+            },
             headers: {
-            'X-API-Key': import.meta.env.VITE_API_KEY,
-            'Content-Type': 'application/json',
-        },
+                'X-API-Key': import.meta.env.VITE_API_KEY,
+                'Content-Type': 'application/json',
+            },
         });
-} catch (error) {
-    console.error(error);
-}
+        info.value = response.data;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 onMounted(async () => {
@@ -55,7 +64,6 @@ onMounted(async () => {
         token = response.data.token.uid;
         iframeSrc.value = `https://webapp.tchek.fr/fr/pwa/sso/intro-shoot-inspect?token=${token}`;
         rapportSrc.value = `https://webapp.tchek.fr/fr/report?token=${token}`;
-        console.log(token);
     } catch (error) {
         console.error(error);
     }
