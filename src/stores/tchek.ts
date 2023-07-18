dc8becf8398455a4cb1d6a56e60aef706549aa6e
kbegot <begot.kevin@gmail.com>
2023-07-18 16:37:30 +0200

    [ADD] new filter store for map data

--- src/stores/tchek.ts
+++ src/stores/tchek.ts
@@ -60,4 +60,3 @@ export const useVehicleStore = defineStore({
     
   }  
 });
-
--- src/views/Rapport.vue
+++ src/views/Rapport.vue
@@ -5,7 +5,7 @@
     <div v-for="(damage, index) in vehiculeStore.damage" :key="index">
       <p>Id: {{ damage.id }}</p>
       <p>Détéction automatique: {{ damage.isAuto }}</p>
-      <p>Localisation: {{ damage.location }} </p>
+      <p>Localisation: {{ getLocationName(damage.location) }}</p>
       <p>Severity: {{ damage.severity }}</p>
       <br>
     </div>
@@ -14,6 +14,17 @@
 
 <script setup lang="ts">
 import { useVehicleStore } from '@/stores/tchek';
+import { uselocationFilter } from '@/stores/filter';
 
 const vehiculeStore = useVehicleStore();
+const locationFilter = uselocationFilter();
+
+const getLocationName = (locationCode: string): string => {
+  if (locationFilter.data === null) {
+    return '';
+  }
+
+  const foundLocation = locationFilter.data.find(location => location.code === locationCode);
+  return foundLocation ? foundLocation.name : '';
+};
 </script>
