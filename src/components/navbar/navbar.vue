f5a1cf5ec53f7ea08ed2b165751f0f3944d7a2c0
kbegot <begot.kevin@gmail.com>
2023-08-02 17:41:13 +0200

    [ADD] tailwind config

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -1,27 +1,28 @@
 <template>
   <header class="fixed top-0 left-0 w-[100%]">
     <!-- Mobile menu (hamburger) -->
-    <nav class="top-0 items-center justify-between w-full px-3 py-5 text-center text-white bg-primary md:hidden">
-      <button @click="toggleMenu" class="focus:outline-none">
-        <img src="../../assets/menu.svg" :class="{'block': !isMenuOpen, 'hidden': isMenuOpen}">
+    <nav class="top-0 items-center justify-between w-full h-16 px-3 py-5 text-center text-white bg-primary md:hidden">
+      <button @click="toggleMenu" class="flex items-center justify-end w-full focus:outline-none">
+        <img src="../../assets/menu.svg" :class="{'block': !isMenuOpen, 'hidden': isMenuOpen}" class="w-7">
+        <img src="../../assets/error.svg" :class="{'block': isMenuOpen, 'hidden': !isMenuOpen}" class="w-7" @click.stop="toggleMenu">
       </button>
     </nav>
 
     <!-- Desktop menu -->
-    <nav class="top-0 items-center justify-between hidden w-full px-3 py-5 text-center text-white bg-primary md:flex">
+    <nav class="top-0 items-center justify-between hidden w-full h-16 px-3 py-5 text-center text-white bg-primary md:flex">
       <RouterLink v-for="link in links" :key="link.name" @click="toggleMenu" class="block hover:text-red-300" :to="{ name: link.name }">{{ link.label }}</RouterLink>
     </nav>
 
     <!-- Mobile menu items -->
     <div :class="{'hidden': !isMenuOpen, 'block': isMenuOpen}" class="w-full h-screen text-white md:hidden bg-primary">
-      <div class="px-3 py-5">
-        <RouterLink v-for="link in links" :key="link.name" @click="toggleMenu" class="block py-2 hover:text-red-300" :to="{ name: link.name }">{{ link.label }}</RouterLink>
+      <div class="menu-transition">
+        <div class="flex flex-col items-center px-3 py-5">
+          <RouterLink v-for="link in links" :key="link.name" @click="toggleMenu" class="block py-2 hover:text-red-300" :to="{ name: link.name }">{{ link.label }}</RouterLink>
+        </div>
       </div>
-      <img src="../../assets/error.svg" :class="{'block': isMenuOpen, 'hidden': !isMenuOpen}" class="w-4" @click.stop="toggleMenu">
     </div>
   </header>
 </template>
-
 <script setup>
 import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
@@ -38,3 +39,21 @@ const links = [
   { name: 'rapport', label: 'Rapport' },
 ];
 </script>
+<style>
+
+.menu-transition {
+  animation-name: slideDown;
+  animation-duration: 1s;
+}
+
+@keyframes slideDown {
+  from {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,43 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-content: [
-  // Example content paths...
-  './public/**/*.html',
-  './src/**/*.{js,jsx,ts,tsx,vue}',
-],
-theme: {
-  // ...
-}
-// ...
-}
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      xs: '384px',
+      // => @media (min-width: 384px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#de2630',
+        secondary: '#33A595',
+
+      },
+      textColor: (theme) => theme('accent'),
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
